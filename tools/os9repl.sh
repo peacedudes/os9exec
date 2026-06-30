40703209e6c2859f710442f0ff9324ece3ff3fc5
Robert Doggett <robert@peacedudes.com>
2026-06-30 02:42:08 -0700

    Fix: auto-set TERM=vt100 in os9repl.sh on startup

--- tools/os9repl.sh
+++ tools/os9repl.sh
@@ -85,6 +85,8 @@ cmd_start() {
     tmux new-session -d -s "$SESSION" -c "$REPO" -x 220 -y 60 "OS9DISK='$REPO/dd' ./os9exec /dd/CMDS/shell"
     printf '[starting os9exec...]\n'
     if wait_prompt; then
+        tmux send-keys -t "$SESSION" "setenv TERM vt100" Enter
+        wait_prompt
         printf '[ready]\n'
         pane | grep -v '^[[:space:]]*$' | tail -5
     fi
