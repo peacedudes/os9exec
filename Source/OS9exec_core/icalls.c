71a1943d82229e448d1d9d4784be59d3a66eabb4
Robert Doggett <robert@peacedudes.com>
2026-07-14 15:07:04 -0700

    Fix: guard guest null buffers in I$Read/I$Write/I$ReadLn/I$WritLn and /L2 getstat (E_BPADDR)

--- Source/OS9exec_core/icalls.c
+++ Source/OS9exec_core/icalls.c
@@ -326,6 +326,7 @@ os9err OS9_I_WritLn( regs_type *rp, ushort cpid )
     path= loword(rp->d[0]);
     cnt =        rp->d[1];
     buff= (char*)FROM68K(rp->a[0]);
+    if (buff==NULL && cnt>0) return os9error(E_BPADDR); /* a0 = write buffer; 0 with a length is a bad address */
 
     path= path & 0x7f; /* mask the specific OS9exec non-debug flag */
    
@@ -376,6 +377,7 @@ os9err OS9_I_Write( regs_type *rp, ushort cpid )
     path= loword(rp->d[0]);
     cnt =        rp->d[1];
     buff= (char*)FROM68K(rp->a[0]);
+    if (buff==NULL && cnt>0) return os9error(E_BPADDR); /* a0 = write buffer; 0 with a length is a bad address */
 
     /* now write */
     err= usrpath_write(cpid,path, &cnt,buff, false);
@@ -415,6 +417,7 @@ os9err OS9_I_ReadLn( regs_type *rp, ushort cpid )
     p=(char *)  FROM68K(rp->a[0]);
     path=loword(rp->d[0]);
     cnt =       rp->d[1];
+    if (p==NULL && cnt>0) return os9error(E_BPADDR); /* a0 = read buffer; 0 with a length is a bad address */
 
     err= usrpath_read(cpid,path,&cnt,p,true); if (err) return err;
     rp->d[1]= cnt; /* return # of chars actually read */
@@ -448,6 +451,7 @@ os9err OS9_I_Read( regs_type *rp, ushort cpid )
    p=(char *)  FROM68K(rp->a[0]);
    path=loword(rp->d[0]);
    cnt =       rp->d[1];
+   if (p==NULL && cnt>0) return os9error(E_BPADDR); /* a0 = read buffer; 0 with a length is a bad address */
 
    err=usrpath_read(cpid,path,&cnt,p,false); if (err) return err;
    rp->d[1]= cnt; /* return # of chars actually read */
@@ -607,7 +611,7 @@ os9err OS9_I_Attach( regs_type *rp, _pid_ )
 {
 	char* name= (char*) FROM68K(rp->a[0]);
 
-	if (ustrcmp( name,"/L2" )==0) {
+	if (name!=NULL && ustrcmp( name,"/L2" )==0) { /* a0=0: don't strcmp a NULL name */
 		rp->a[2]= 0x22002200; /* %%% /L2 identifier */
 		return 0;
 	} /* if */
